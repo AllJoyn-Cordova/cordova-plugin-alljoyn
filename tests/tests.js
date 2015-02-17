93c7cf3ae8877804ede8a0755af2343fef228980
Ville Rantala <ville.rantala@iki.fi>
2015-02-17 14:27:44 +0200

    Added first bus connection test.

--- tests/tests.js
+++ tests/tests.js
@@ -4,4 +4,14 @@ exports.defineAutoTests = function() {
       expect(window.AllJoyn).toBeDefined();
     });
   });
+
+  describe('Connecting to bus', function() {
+    it("bus should be returned after connecting", function(done) {
+      AllJoyn.connect(function(bus) {
+        expect(bus.addListener).toBeDefined();
+        // TODO: Check also other members of the bus object
+        done();
+      });
+    });
+  });
 };
