ad0a65fc7f4595c7af3e0b7fad76b66b3124516e
Ville Rantala <ville.rantala@iki.fi>
2015-03-09 16:19:49 +0200

    Coding style alignment.
    Move handler to the top of the file so that it is defined
    before it is used.

--- src/windows/AllJoynProxy.js
+++ src/windows/AllJoynProxy.js
@@ -1,8 +1,154 @@
+/*
+ * To use the handler, you must have first connected to an AllJoyn bus. The handler is then started
+ * by passing a reference to your bus attachment.
+ *
+ * AllJoynMessageHandler.start(<your bus attachment>);
+ *
+ * To register handlers, you call the addHandler by passing the message ids you are interested,
+ * the signature of your message and the callback function that gets called if such messages arrive.
+ * The signature is an AllJoyn-specific value that is used to unmarshal the body of the message.
+ *
+ * AllJoynMessageHandler.addHandler(<interesting message id>,
+ *                                  <return value signature>
+ *                                  function(messageObject, messageBody) {
+ *                                    // handle the received message
+ *                                  });
+ */
+var messageHandler = (function() {
+  // Use 0 as unmarshal timeout so that we don't end up blocking
+  // the UI while waiting for new messages
+  var AJ_UNMARSHAL_TIMEOUT = 0;
+  var AJ_METHOD_TIMEOUT = 1000 * 1;
+  var AJ_MESSAGE_SLOW_LOOP_INTERVAL = 500;
+  var AJ_MESSAGE_FAST_LOOP_INTERVAL = 50;
+
+  var messageHandler = {};
+  var messageListeners = {};
+  var interval = null;
+
+  messageHandler.start = function(busAttachment) {
+    // Flag to store current interval pace
+    var runningFast;
+    // This function can be called to update the interval based on if
+    // the bus attachment had new messages or now. The idea is that if there are
+    // messages, we run the loop faster to 'flush the bus' and slower if there is
+    // nothing new.
+    var updateInterval = function(unmarshalStatus) {
+      if (unmarshalStatus === AllJoynWinRTComponent.AJ_Status.aj_OK) {
+        if (!runningFast) {
+          clearInterval(interval);
+          runningFast = true;
+          interval = setInterval(handlerFunction, AJ_MESSAGE_FAST_LOOP_INTERVAL);
+        }
+      }
+      if (unmarshalStatus === AllJoynWinRTComponent.AJ_Status.aj_ERR_TIMEOUT) {
+        if (runningFast) {
+          clearInterval(interval);
+          runningFast = false;
+          interval = setInterval(handlerFunction, AJ_MESSAGE_SLOW_LOOP_INTERVAL);
+        }
+      }
+    };
+    var handlerFunction = function() {
+      var aj_message = new AllJoynWinRTComponent.AJ_Message();
+      AllJoynWinRTComponent.AllJoyn.aj_UnmarshalMsg(busAttachment, aj_message, AJ_UNMARSHAL_TIMEOUT).done(function(status) {
+        if (status === AllJoynWinRTComponent.AJ_Status.aj_OK) {
+          var messageObject = aj_message.get();
+          var receivedMessageId = messageObject.msgId;
+          // Check if we have listeners for this message id
+          if (messageListeners[receivedMessageId]) {
+            // Pass the value to listeners
+            var callbacks = messageListeners[receivedMessageId];
+            for (var i = 0; i < callbacks.length; i += 1) {
+              var signature = callbacks[i][0];
+              var response = null;
+              // Unmarshal the message arguments
+              AllJoynWinRTComponent.AllJoyn.aj_UnmarshalArgsWithDelegate(aj_message, signature, function(unmarshalArgsStatus, messageArgs) {
+                if (unmarshalArgsStatus === AllJoynWinRTComponent.AJ_Status.aj_OK) {
+                  console.log('Unmarshaling of arguments from message with id ' + receivedMessageId + ' succeeded');
+
+                  // The messageArgs is an object array created in the Windows Runtime Component
+                  // so turn that to a JavaScript array before returning it. Since there can be
+                  // nested container types we need to do this recursively.
+
+                  var getClass = {}.toString;
+                  var convertArgs = function (itemOrCollection) {
+                    // Check for the collection type we return for containers
+                    if (getClass.call(itemOrCollection) === '[object Windows.Foundation.Collections.IObservableVector`1<Object>]') {
+                      var subArray = [];
+                      for (var j = 0; j < itemOrCollection.length; j += 1) {
+                        subArray.push(convertArgs(itemOrCollection[j]));
+                      }
+                      return subArray;
+                    } else {
+                        return itemOrCollection;
+                    }
+                  };
+                  response = convertArgs(messageArgs);
+                } else {
+                  console.log('Unmarshaling of arguments from message with id ' + receivedMessageId + ' failed with status ' + unmarshalArgsStatus);
+                }
+
+                if (receivedMessageId === AllJoynWinRTComponent.AJ_Std.aj_Method_Accept_Session) {
+                  callbacks[i][1](messageObject, response, aj_message, function() {
+                    // When request handled, close message and continue with the handler loop
+                    AllJoynWinRTComponent.AllJoyn.aj_CloseMsg(aj_message);
+                    messageHandler.start(busAttachment);
+                  });
+                  // Stop the message loop until accept session request is handled
+                  messageHandler.stop();
+                  return;
+                } else {
+                  callbacks[i][1](messageObject, response);
+                  AllJoynWinRTComponent.AllJoyn.aj_CloseMsg(aj_message);
+                }
+              });
+            }
+          } else {
+            console.log('Message with id ' + receivedMessageId + ' passed to default handler');
+            AllJoynWinRTComponent.AllJoyn.aj_BusHandleBusMessage(aj_message);
+            AllJoynWinRTComponent.AllJoyn.aj_CloseMsg(aj_message);
+          }
+        }
+        updateInterval(status);
+      });
+    };
+    // Initially start with slower interval
+    runningFast = false;
+    interval = setInterval(handlerFunction, AJ_MESSAGE_SLOW_LOOP_INTERVAL);
+  };
+
+  messageHandler.stop = function() {
+    clearInterval(interval);
+  };
+
+  messageHandler.addHandler = function(messageId, signature, callback) {
+    // Create a list of handlers for this message id if it doesn't exist yet
+    if (typeof messageListeners[messageId] !== 'object') {
+      messageListeners[messageId] = [];
+    }
+    messageListeners[messageId].push([signature, callback]);
+  };
+
+  messageHandler.removeHandler = function(messageId, callback) {
+    messageListeners[messageId] = messageListeners[messageId].filter(
+      function(element) {
+        // Filter out the given callback function
+        return (element[1] !== callback);
+      }
+    );
+  };
+
+  return messageHandler;
+})();
+
+// This initialization call is required once before doing
+// other AllJoyn operations.
+AllJoynWinRTComponent.AllJoyn.aj_Initialize();
+
 var AJ_CONNECT_TIMEOUT = 1000 * 5;
 var AJ_BUS_START_FINDING = 0;
 var AJ_BUS_STOP_FINDING = 1;
-
-var METHOD_ACCEPT_SESSION_ID = AllJoynWinRTComponent.AJ_Std.aj_Method_Accept_Session;
 
 var busAttachment = new AllJoynWinRTComponent.AJ_BusAttachment();
 
@@ -329,7 +475,7 @@ cordova.commandProxy.add('AllJoyn', {
   setAcceptSessionListener: function(success, error, parameters) {
     var acceptSessionListener = parameters[0];
 
-    var acceptSessionId = METHOD_ACCEPT_SESSION_ID;
+    var acceptSessionId = AllJoynWinRTComponent.AJ_Std.aj_Method_Accept_Session;
     messageHandler.addHandler(
       acceptSessionId, 'qus',
       function(messageObject, messageBody, messagePointer, doneCallback) {
@@ -351,150 +497,3 @@ cordova.commandProxy.add('AllJoyn', {
   }
 });
 
-// This initialization call is required once before doing
-// other AllJoyn operations.
-AllJoynWinRTComponent.AllJoyn.aj_Initialize();
-
-/*
- * To use the handler, you must have first connected to an AllJoyn bus. The handler is then started
- * by passing a reference to your bus attachment.
- *
- * AllJoynMessageHandler.start(<your bus attachment>);
- *
- * To register handlers, you call the addHandler by passing the message ids you are interested,
- * the signature of your message and the callback function that gets called if such messages arrive.
- * The signature is an AllJoyn-specific value that is used to unmarshal the body of the message.
- *
- * AllJoynMessageHandler.addHandler(<interesting message id>,
- *                                  <return value signature>
- *                                  function(messageObject, messageBody) {
- *                                    // handle the received message
- *                                  });
- */
-var messageHandler = (function() {
-  // Use 0 as unmarshal timeout so that we don't end up blocking
-  // the UI while waiting for new messages
-  var AJ_UNMARSHAL_TIMEOUT = 0;
-  var AJ_METHOD_TIMEOUT = 1000 * 1;
-  var AJ_MESSAGE_SLOW_LOOP_INTERVAL = 500;
-  var AJ_MESSAGE_FAST_LOOP_INTERVAL = 50;
-
-  var messageHandler = {};
-  var messageListeners = {};
-  var interval = null;
-
-  messageHandler.start = function(busAttachment) {
-    // Flag to store current interval pace
-    var runningFast;
-    // This function can be called to update the interval based on if
-    // the bus attachment had new messages or now. The idea is that if there are
-    // messages, we run the loop faster to 'flush the bus' and slower if there is
-    // nothing new.
-    var updateInterval = function(unmarshalStatus) {
-      if (unmarshalStatus === AllJoynWinRTComponent.AJ_Status.aj_OK) {
-        if (!runningFast) {
-          clearInterval(interval);
-          runningFast = true;
-          interval = setInterval(handlerFunction, AJ_MESSAGE_FAST_LOOP_INTERVAL);
-        }
-      }
-      if (unmarshalStatus === AllJoynWinRTComponent.AJ_Status.aj_ERR_TIMEOUT) {
-        if (runningFast) {
-          clearInterval(interval);
-          runningFast = false;
-          interval = setInterval(handlerFunction, AJ_MESSAGE_SLOW_LOOP_INTERVAL);
-        }
-      }
-    };
-    var handlerFunction = function() {
-      var aj_message = new AllJoynWinRTComponent.AJ_Message();
-      AllJoynWinRTComponent.AllJoyn.aj_UnmarshalMsg(busAttachment, aj_message, AJ_UNMARSHAL_TIMEOUT).done(function(status) {
-        if (status === AllJoynWinRTComponent.AJ_Status.aj_OK) {
-          var messageObject = aj_message.get();
-          var receivedMessageId = messageObject.msgId;
-          // Check if we have listeners for this message id
-          if (messageListeners[receivedMessageId]) {
-            // Pass the value to listeners
-            var callbacks = messageListeners[receivedMessageId];
-            for (var i = 0; i < callbacks.length; i += 1) {
-              var signature = callbacks[i][0];
-              var response = null;
-              // Unmarshal the message arguments
-              AllJoynWinRTComponent.AllJoyn.aj_UnmarshalArgsWithDelegate(aj_message, signature, function(unmarshalArgsStatus, messageArgs) {
-                if (unmarshalArgsStatus === AllJoynWinRTComponent.AJ_Status.aj_OK) {
-                  console.log('Unmarshaling of arguments from message with id ' + receivedMessageId + ' succeeded');
-
-                  // The messageArgs is an object array created in the Windows Runtime Component
-                  // so turn that to a JavaScript array before returning it. Since there can be
-                  // nested container types we need to do this recursively.
-
-                  var getClass = {}.toString;
-                  var convertArgs = function (itemOrCollection) {
-                    // Check for the collection type we return for containers
-                    if (getClass.call(itemOrCollection) === '[object Windows.Foundation.Collections.IObservableVector`1<Object>]') {
-                      var subArray = [];
-                      for (var j = 0; j < itemOrCollection.length; j += 1) {
-                        subArray.push(convertArgs(itemOrCollection[j]));
-                      }
-                      return subArray;
-                    } else {
-                        return itemOrCollection;
-                    }
-                  };
-                  response = convertArgs(messageArgs);
-                } else {
-                  console.log('Unmarshaling of arguments from message with id ' + receivedMessageId + ' failed with status ' + unmarshalArgsStatus);
-                }
-
-                if (receivedMessageId === METHOD_ACCEPT_SESSION_ID) {
-                  callbacks[i][1](messageObject, response, aj_message, function() {
-                    // When request handled, close message and continue with the handler loop
-                    AllJoynWinRTComponent.AllJoyn.aj_CloseMsg(aj_message);
-                    messageHandler.start(busAttachment);
-                  });
-                  // Stop the message loop until accept session request is handled
-                  messageHandler.stop();
-                  return;
-                } else {
-                  callbacks[i][1](messageObject, response);
-                  AllJoynWinRTComponent.AllJoyn.aj_CloseMsg(aj_message);
-                }
-              });
-            }
-          } else {
-            console.log('Message with id ' + receivedMessageId + ' passed to default handler');
-            AllJoynWinRTComponent.AllJoyn.aj_BusHandleBusMessage(aj_message);
-            AllJoynWinRTComponent.AllJoyn.aj_CloseMsg(aj_message);
-          }
-        }
-        updateInterval(status);
-      });
-    };
-    // Initially start with slower interval
-    runningFast = false;
-    interval = setInterval(handlerFunction, AJ_MESSAGE_SLOW_LOOP_INTERVAL);
-  };
-
-  messageHandler.stop = function() {
-    clearInterval(interval);
-  };
-
-  messageHandler.addHandler = function(messageId, signature, callback) {
-    // Create a list of handlers for this message id if it doesn't exist yet
-    if (typeof messageListeners[messageId] !== 'object') {
-      messageListeners[messageId] = [];
-    }
-    messageListeners[messageId].push([signature, callback]);
-  };
-
-  messageHandler.removeHandler = function(messageId, callback) {
-    messageListeners[messageId] = messageListeners[messageId].filter(
-      function(element) {
-        // Filter out the given callback function
-        return (element[1] !== callback);
-      }
-    );
-  };
-
-  return messageHandler;
-})();
